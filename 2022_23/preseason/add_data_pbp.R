648185932cfe19241e0e2538f8c1e13abe429c37
ramirobentes <ramirobentes@live.com>
2022-10-17 13:36:50 -0300

    Create add_data_pbp.R

---
+++ 2022_23/preseason/add_data_pbp.R
@@ -0,0 +1,6 @@
+# corrections to pbp ------------------------------------------------------
+
+corrections <- tribble(
+  ~game_id,     ~clock,   ~secs_passed_game, ~number_event, ~msg_type, ~act_type,                                              ~description,
+  12200026,    "09:59",                1561,           249,         6,         1,             "[SAS] Jones Foul: Personal (2 PF) (B Nansel)"
+  )
