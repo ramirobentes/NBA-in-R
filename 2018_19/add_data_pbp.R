2242d4d36e9af2f0c4af642be2b6e8f1dfc7d4c5
ramirobentes <ramirobentes@live.com>
2022-10-24 17:20:11 -0300

    Create add_data_pbp.R

---
+++ 2018_19/add_data_pbp.R
@@ -0,0 +1,13 @@
+missing_starters <- tribble(
+  ~game_id,      ~period,      ~slug_team,          ~player_name,
+  21800143,            6,           "CHI",      "Justin Holiday",
+  21800143,            6,           "NYK",         "Noah Vonleh",
+  21800216,            5,           "BOS",   "Marcus Morris Sr.",
+  21800276,            3,           "DEN", "Juancho Hernangomez",
+  21800371,            5,           "BKN",          "Joe Harris",
+  21800565,            5,           "HOU",         "P.J. Tucker",
+  21800619,            5,           "OKC",   "Terrance Ferguson",
+  21800881,            5,           "UTA",          "Joe Ingles",
+  21801070,            5,           "MEM",       "Bruno Caboclo",
+  21801132,            5,           "GSW",      "Andre Iguodala",
+  21801229,            5,           "UTA",     "Tyler Cavanaugh")
