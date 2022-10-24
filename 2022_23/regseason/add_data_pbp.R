bef23154a68d807850f57a52aa8978949d2503ef
ramirobentes <ramirobentes@live.com>
2022-10-24 13:14:25 -0300

    Update add_data_pbp.R

--- 2022_23/regseason/add_data_pbp.R
+++ 2022_23/regseason/add_data_pbp.R
@@ -1,3 +1,5 @@
 missing_starters <- tribble(
   ~game_id,      ~period,      ~slug_team,      ~player_name,
-  22200025,            5,           "MIN",  "Jaden McDaniels")
+  22200025,            5,           "MIN",  "Jaden McDaniels",
+  22200039,            5,           "WAS",     "Delon Wright",
+  22200040,            5,           "UTA",      "Mike Conley")
