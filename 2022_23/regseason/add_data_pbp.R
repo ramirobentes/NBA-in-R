59dcdfeaa6b8ba35b4b41cea3e18fa5ac2ca1037
ramirobentes <ramirobentes@live.com>
2022-11-03 14:46:42 -0300

    Update add_data_pbp.R

--- 2022_23/regseason/add_data_pbp.R
+++ 2022_23/regseason/add_data_pbp.R
@@ -3,4 +3,6 @@ missing_starters <- tribble(
   22200025,            5,           "MIN",  "Jaden McDaniels",
   22200039,            5,           "WAS",     "Delon Wright",
   22200040,            5,           "UTA",      "Mike Conley",
-  22200072,            5,           "BOS",       "Al Horford")
+  22200072,            5,           "BOS",       "Al Horford",
+  22200117,            5,           "NOP",    "Naji Marshall",
+  22200117,            5,           "LAL",    "Austin Reaves")
