753a5c74129ac5f3fab959a7d3f8f68c6c26866d
ramirobentes <ramirobentes@live.com>
2023-02-21 15:53:49 -0300

    Create add_data_pbp.R

---
+++ 2017_18/add_data_pbp.R
@@ -0,0 +1,14 @@
+missing_starters <- tribble(
+  ~game_id,      ~period,      ~slug_team,          ~player_name,
+  21700064,            5,           "WAS",     "Kelly Oubre Jr.",
+  21700249,            5,           "BOS",          "Al Horford",
+  21700482,            5,           "BKN",        "Allen Crabbe",
+  21700584,            2,           "NOP",    "Dante Cunningham",
+  21700607,            5,           "NYK",     "Michael Beasley",
+  21700635,            5,           "NOP",           "Ian Clark",
+  21700692,            6,           "NOP",       "Darius Miller",
+  21700893,            5,           "MIL",        "Eric Bledsoe",
+  21700966,            5,           "TOR",          "Kyle Lowry",
+  21701103,            5,           "CHA",        "Dwayne Bacon",
+  21701103,            5,           "NYK",        "Courtney Lee",
+  21701136,            5,           "MIL",         "Jason Terry")
