a6b667fdf11a0e36e7a48915df181a3c5ac894d3
ramirobentes <ramirobentes@live.com>
2023-02-22 13:48:51 -0300

    Create add_data_pbp.R

---
+++ 2020_21/add_data_pbp.R
@@ -0,0 +1,12 @@
+missing_starters <- tribble(
+  ~game_id,      ~period,      ~slug_team,          ~player_name,
+  22000023,            5,           "DET",      "Delon Wright",
+  22000100,            5,           "IND",         "Justin Holiday",
+  22000120,            5,           "DEN",   "Gary Harris",
+  22000440,            5,           "MIN",           "Anthony Edwards",
+  22000465,            5,           "NOP",          "Lonzo Ball",
+  22000485,            1,           "DAL",   "Dorian Finney-Smith",
+  22000637,            5,           "CHI",          "Coby White",
+  22000645,            5,           "IND",          "T.J. McConnell",
+  22001012,            5,           "WAS",       "Raul Neto",
+  22001064,            5,           "CHA",      "Jalen McDaniels")
